ce9de9e3eb7b62e3da28752ec6b55258dc379ef9
unknown <JPAcyber@Latihan32.jpa.gov.my>
2014-06-10 15:50:12 +0800

    testing

---
+++ form4.php
@@ -0,0 +1,23 @@
+<?php
+//if ($_POST["hantar"]) {
+	$nama = $_POST["nama"];
+	$tarikh = $_POST["date"];
+	$jantina = $_POST["jantina"];
+    $membaca = $_POST["membaca"];
+    $menulis = $_POST["menulis"];
+    $mengira = $_POST["mengira"];
+    $jp = $_POST["jp"];
+    $komen = $_POST["textarea"];
+	
+echo "nama: $nama <br />
+      tarikh lahir : $tarikh<br />
+      jantina : $jantina <br />
+      membaca : $membaca <br />
+      menulis : $menulis <br />
+      mengira :$mengira <br />
+      jenis pekerjaan : $jp <br />
+      komen : $komen";
+      
+      
+// }
+?>
